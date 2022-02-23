d570946656984490979a4cf3ac31b111df731d02
Lu Yang <74538732+Luyabs@users.noreply.github.com>
2022-02-23 08:33:06 +0800

    Update Calculate.h

--- Code/Calculate.h
+++ Code/Calculate.h
@@ -4,13 +4,13 @@
 #include "LinkStack.h"			//栈用于给变量赋值
 #include <string>
 
-int OperPrior(char op1, char op2);
+int OperPrior(char op1, char op2);					//求运算符优先级
 bool IsLeaf(const BinTreeNode<string>* r);			//是否叶子节点
-bool IsOperator(char ch);       // 操作结果：判断ch是否为操作符 
-LinkList<dictionary> DictBuilding(int num=0);
-double DictFind(const LinkList<dictionary>& dict, const char& ch);
-void InfixInToPostfix(string* in, string* post, int& n);
+int CountVariable(const BinaryTree<string> r);	//非递归遍历 统计变量个数
+bool IsOperator(char ch);							//是否是操作符
+LinkList<dictionary> DictBuilding(int num=0);		//建立字典
+double DictFind(const LinkList<dictionary>& dict, const char& ch);		//依照字典键查找值
+void InfixInToPostfix(string* in, string* post, int& n);	
 string Operate(const string& first, const string& op, const string& second, const LinkList<dictionary>& dict);	// 四则运算(支持变量与常量): R(string1,string2) = F(num1,num2) = num3 → string3
 string Calculate(const BinTreeNode<string>* r, const LinkList<dictionary>& dict);		//读二叉树中表达式做运算
 double Calculate(const BinaryTree<string>& tree, const LinkList<dictionary>& dict);		//读二叉树中表达式做运算
-
